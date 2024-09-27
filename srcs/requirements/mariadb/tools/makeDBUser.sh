f4aefefcbcbaca477662dd6321bdf53adfc3334a
agengemb <32024540+UhtredTheDane@users.noreply.github.com>
2024-09-27 17:41:07 +0200

    Update makeDBUser.sh
    Ajout et correction des fautes de frappe

--- srcs/requirements/mariadb/tools/makeDBUser.sh
+++ srcs/requirements/mariadb/tools/makeDBUser.sh
@@ -1,8 +1,8 @@
-service mysql start
+service mysql start;
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILIGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PQSSWORD}';"
+mysql -e "GRANT ALL PRIVILIGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-
+exec mysqld_safe
