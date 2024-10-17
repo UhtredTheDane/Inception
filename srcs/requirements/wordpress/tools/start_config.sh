92ab6e08d81112453ff83348392349c9a19959c6
UhtredTheDane <gengembre.alexis@gmail.com>
2024-10-17 17:38:46 +0200

    fin container wordpress + debut docker compose

---
+++ srcs/requirements/wordpress/tools/start_config.sh
@@ -0,0 +1,12 @@
+if [ ! -f /var/www/wordpress/wp-config.php ]; then
+	sleep 15
+	wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'
+	wp core install --allow-root --url=$WP_SITE --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --path='/var/www/wordpress'
+	wp user create --allow-root "$WP_USER" "$WP_USER_EMAIL" --role=author --user_pass=$WP_USER_PASSWORD --path='/var/www/wordpress'
+fi
+
+if [ ! -d /run/php ]; then
+	mkdir -p /run/php
+fi
+
+exec php-fpm7.4 -F
