b50cf46e13b9cdc2204e7b3844b9e415c6f7a84f
agengemb <32024540+UhtredTheDane@users.noreply.github.com>
2024-10-17 14:46:57 +0200

    Update makeDBUser.sh

--- srcs/requirements/mariadb/tools/makeDBUser.sh
+++ srcs/requirements/mariadb/tools/makeDBUser.sh
@@ -4,5 +4,5 @@ mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '$
 mysql -e "GRANT ALL PRIVILIGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+mysqladmin -u root -p $SQL_ROOT_PASSWORD shutdown
 exec mysqld_safe
