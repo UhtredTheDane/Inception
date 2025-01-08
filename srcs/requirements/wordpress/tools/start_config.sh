02e685ec8f8c9ac8bc3a33989fd1e4216c4ae75a
UhtredTheDane <gengembre.alexis@gmail.com>
2025-01-08 02:14:19 +0100

    wordpress ok, fix nginx

--- srcs/requirements/wordpress/tools/start_config.sh
+++ srcs/requirements/wordpress/tools/start_config.sh
@@ -1,4 +1,6 @@
-sleep 5
+sleep 10
+#service php7.4-fpm start
+cd /var/www/wordpress
 if [ ! -f /var/www/wordpress/wp-config.php ]; then
 	wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'
 	until wp db check --allow-root --path='/var/www/wordpress'; do
@@ -7,11 +9,10 @@ if [ ! -f /var/www/wordpress/wp-config.php ]; then
 	done
 	wp core install --allow-root --url=$WP_SITE --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --path='/var/www/wordpress'
 	wp user create --allow-root "$WP_USER" "$WP_USER_EMAIL" --role=author --user_pass=$WP_USER_PASSWORD --path='/var/www/wordpress'
-echo "ici"
 fi
-echo "et la ?"
+#service php7.4-fpm stop
 if [ ! -d /run/php ]; then
 	mkdir -p /run/php
 fi
 echo "on est a la fin ?"
-exec php-fpm7.4 -F
+/usr/sbin/php-fpm7.4 -F
